397a7cf703fd0c9b280be1d4f28667ae7c81b794
fragtastic <fragtastic@users.noreply.github.com>
2016-01-29 01:54:27 -0800

    Initial Commit Basic layout and structure

---
+++ calpolycss.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+app.debug = False
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+
+@app.route('/<page>.html')
+def page(page):
+    return render_template(page + ".html")
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
